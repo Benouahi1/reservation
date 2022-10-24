df6a812b90d95b196748b115f2687d4ba563d3b9
ayoub benouahi <abenouahi5@gmail.com>
2022-10-24 19:27:53 +0100

    Card de car

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import './Home.css';
 import Img1 from "../../images/juan-encalada-6mcVaoGNz1w-unsplash.jpg";
 import Img2 from "../../images/jonathan-borba-T5jzpRTVF1U-unsplash.jpg";
+import bus from "../../images/right-arrow-removebg-preview.png";
 
 
 function Home(){
@@ -25,17 +26,71 @@ function Home(){
                 </div>
                 <div className="InfoVoyages">
                     <div className="Card ">
-                    <img className="img1" src={Img2}></img>
+                    <img className="img2" src={Img2}></img>
                         <div className="info">
-
+                            <p className="Ville">Rabat</p>
+                            <img className="slach" src={bus}></img>
+                            <p className="Ville">Fes</p>
+                        </div>
+                        <div className="info">
+                            <p className="Ville">Prix :200 DH</p>
+                            <p className="Ville">Date :29/10/2022</p>
+                        </div>
+                        <div className="info">
+                            <p className="Ville">Hours : 11:00 AM</p>
                         </div>
                     </div>
                     <div className="Card">
+                    <img className="img2" src={Img2}></img>
                         <div className="info">
-
+                            <p className="Ville">Khemisset</p>
+                            <img className="slach" src={bus}></img>
+                            <p className="Ville">Tanger</p>
+                        </div>
+                        <div className="info">
+                            <p className="Ville">Prix :300 DH</p>
+                            <p className="Ville">Date :31/10/2022</p>
+                        </div>
+                        <div className="info">
+                            <p className="Ville">Hours : 10:00 AM</p>
                         </div>
                     </div>
+                    <div className="Card">
+                    <img className="img2" src={Img2}></img>
+                        <div className="info">
+                            <p className="Ville">Meknes</p>
+                            <img className="slach" src={bus}></img>
+                            <p className="Ville">Safi</p>
+                        </div>
+                        <div className="info">
+                            <p className="Ville">Prix :200 DH</p>
+                            <p className="Ville">Date :29/10/2022</p>
+                        </div>
+                        <div className="info">
+                            <p className="Ville">Hours : 8:00 AM</p>
+                        </div>
+                    </div>
+                    <div className="Card">
+                    <img className="img2" src={Img2}></img>
+                        <div className="info">
+                            <p className="Ville">Tanger</p>
+                            <img className="slach" src={bus}></img>
+                            <p className="Ville">Safi</p>
+                        </div>
+                        <div className="info">
+                            <p className="Ville">Prix :300 DH</p>
+                            <p className="Ville">Date :24/10/2022</p>
+                        </div>
+                        <div className="info">
+                            <p className="Ville">Hours : 02:00 AM</p>
+                        </div>
+                    </div>
+                </div>
 
+                <div>
+                    <div>
+                        <p className="About">About</p>
+                    </div>
                 </div>
             </div>
     );
