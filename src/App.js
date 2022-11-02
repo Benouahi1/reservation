8f3c6a97c51bc9583bf93da5a633ec158ade9dc2
ayoub benouahi <abenouahi5@gmail.com>
2022-11-02 20:07:25 +0100

    App

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
         <Route path="/Reserve" element={<Reserver />}></Route>
         <Route path="/Login" element={<Login />}></Route>
         <Route path="/Rougister" element={<Rougister />}></Route>
+        
       </Routes>
     </BrowserRouter>
     );
